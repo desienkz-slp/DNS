057578881f9357e77bc84de95b53862875e50b76
desienkz-slp <63319911+desienkz-slp@users.noreply.github.com>
2025-07-23 14:51:10 +0700

    Update update-lists.sh

--- update-lists.sh
+++ update-lists.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+
+echo "🔁 Mengupdate daftar blocklist..."
+
+# Update daftar iklan
 curl -s https://raw.githubusercontent.com/ABPindo/indonesianadblockrules/master/subscriptions/abpindo.txt -o /etc/unbound/blocklist/block-ads.txt
-curl -s  https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -o /etc/unbound/blocklist/block-malware.txt
+
+# Update daftar malware
+curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -o /etc/unbound/blocklist/block-malware.txt
+
+# Update daftar domain dewasa
+curl -s https://raw.githubusercontent.com/alexsannikov/adguardhome-filters/refs/heads/master/porn.txt -o /etc/unbound/blocklist/adult-domains.txt
+
+# Update daftar tambahan dari kamu sendiri
+curl -s -H "Authorization: token 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" "https://raw.githubusercontent.com/desienkz-slp/DNS/main/gen-adult-block.sh" -o /etc/unbound/blocklist/gen-adult-block.sh
+
+
+echo "✅ Semua blocklist diperbarui!"
