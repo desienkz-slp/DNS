36ce49733ffcd7432fc36add3c46e7e005cc921d
desienkz-slp <63319911+desienkz-slp@users.noreply.github.com>
2025-07-29 15:39:55 +0700

    Update gen-adult-block.sh

--- gen-adult-block.sh
+++ gen-adult-block.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
-
 OUT="/etc/unbound/blocklist/adult-redirect.conf"
 REDIRECT_IP="172.18.20.234"
-INPUT="/etc/unbound/blocklist/adult-domains.txt"
-
 > "$OUT"
-declare -A zone_seen
 
-while read -r line; do
-  # Bersihkan dan normalisasi domain
-  domain=$(echo "$line" | xargs | sed -E 's/^\|\|//; s/\^$//; s/\*$//; s/^\*\.?//; s/^www\.//' | tr '[:upper:]' '[:lower:]')
-  [ -z "$domain" ] && continue
-
-  fqdn="${domain}."
-
-  # Hindari duplikat local-zone
-  if [[ -n "${zone_seen[$fqdn]}" ]]; then
-    continue
-  fi
-  zone_seen[$fqdn]=1
-
-  # Tulis konfigurasi redirect
-  echo "local-zone: \"$fqdn\" redirect" >> "$OUT"
-  echo "local-data: \"$fqdn A $REDIRECT_IP\"" >> "$OUT"
-done < "$INPUT"
-
-echo "✅ File selesai dibuat tanpa duplikat dan valid untuk redirect: $OUT"
+while read domain; do
+  echo "local-data: \"$domain A $REDIRECT_IP\"" >> "$OUT"
+  echo "local-data: \"www.$domain A $REDIRECT_IP\"" >> "$OUT"
+done < /etc/unbound/blocklist/adult-domains.txt
